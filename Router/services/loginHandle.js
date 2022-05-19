435c60f27ee2fcf11ab1bb468f893843ad3e0508
Popefrancis24 <francisogbonna24@gmail.com>
2022-05-19 06:32:59 +0100

    resolved issues

--- Router/services/loginHandle.js
+++ Router/services/loginHandle.js
@@ -1,7 +1,7 @@
 const student = (req, res, client) => {
   const {userName, password} = req.body;
-  let query = 'select full_name, reg_no from students where reg_no = $1 and password = $2';
-  client.query(query,[userName, password], (err, result) =>{
+  let query = `select full_name, reg_no from students where reg_no = ${userName} and password = ${password}}`;
+  client.query(query, (err, result) =>{
     if(err) throw err;
     console.log(query);
     console.log(result.rows);
