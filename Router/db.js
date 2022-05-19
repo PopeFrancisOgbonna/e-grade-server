68f429ff7f0a1879db01e2f9a7bc3437dc8d3473
Popefrancis24 <francisogbonna24@gmail.com>
2022-05-19 07:24:09 +0100

    resolved issues

--- Router/db.js
+++ Router/db.js
@@ -1,22 +1,13 @@
 const {Client} = require('pg');
 const isProduction = process.env.NODE_ENV === 'production';
-const connectionStrings = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const client = new Client({
 
-    connectionString: process.env.DATABASE_URL ,
+    connectionString: isProduction? process.env.DATABASE_URL: connectionString,
     ssl: isProduction? {
         rejectUnauthorized: false
     }:false
 });
-const localClient = new Client({
-    connectionString: connectionStrings
-});
-const mainClient = isProduction? client : localClient;
 
-console.log(client.connectionString);
-console.log(isProduction);
-console.log(process.env.DATABASE_URL);
-console.log(connectionStrings);
-console.log(mainClient.database);
 module.exports = client;
